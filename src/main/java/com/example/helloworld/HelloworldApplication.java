7a6caafabbee6c42ea0c511fbaa6a12fd11242c9
DannyYang <yjring@gmail.com>
2023-05-25 14:11:52 +0800

    Update HelloworldApplication.java

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -12,7 +12,7 @@ public class HelloworldApplication {
   class HelloworldController {
     @GetMapping("/")
     String hello() {
-      return "Hello Tekton!! v2";
+      return "Hello Tekton!! v3";
     }
   }
 
